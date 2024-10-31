b350e8ec3adf26719aa5714df45d97287e3ace46
Nirmal7200 <nirmalmanoharan1993@gmail.com>
2024-10-31 09:29:19 +0530

    Added comments

--- hrms-project/src/main/java/com/hrms/hrms_project/HrmsProjectApplication.java
+++ hrms-project/src/main/java/com/hrms/hrms_project/HrmsProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HrmsProjectApplication {
 
+	//Comments
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsProjectApplication.class, args);
 	}
